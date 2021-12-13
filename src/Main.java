9e01c55fea6981e2690fc59492f0288c58c37773
lkuzn <l.kuznetsova95@mail.ru>
2021-12-13 20:25:48 +0400

    Init commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String a = "___________'\"___";
+        String b = "__________---___";
+        String c = "_______/\\_|_|___";
+        String d = "______/__\\|_|___";
+        String e = "_____/____\\_|___";
+        String f = "____/______\\|___";
+        String g = "___/__[__]__\\___";
+        String h = "__/|________|\\__";
+        String i = "_/_|________|_\\_";
+        String j = "___|___---__|___";
+        String k = "___|___| |___|__";
+
+        System.out.println(a + "\n" +b + "\n" +c + "\n" +d + "\n" +e + "\n" +f + "\n" +g + "\n" +h + "\n" +i + "\n" +j + "\n" +k);
+    }
+}
